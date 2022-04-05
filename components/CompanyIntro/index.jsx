162141aff9b46f8034e02d424742507e4b61bf41
RennyKa <nguyenduclong.hy.96@gmail.com>
2022-04-05 16:43:19 +0700

    change some layout

--- components/CompanyIntro/index.jsx
+++ components/CompanyIntro/index.jsx
@@ -1,12 +1,27 @@
 import { Box, Heading, Image, Paragraph } from "grommet";
 import { useRouter } from "next/router";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 function CompanyIntro() {
   const router = useRouter();
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 900px)",
+  });
   return (
-    <Box margin="2em 0" width="80%" height="400px" direction="row" justify="around">
-      <Box width="40%">
+    <Box
+      margin="2em 0"
+      width="80%"
+      align="center"
+      direction={isDesktopOrLaptop ? "row" : "column"}
+      justify="around"
+    >
+      <Image
+        width={isDesktopOrLaptop ? "40%" : "100%"}
+        height="350px"
+        src="images/kisspng-drawing-agriculture-royalty-free-tractor-clip-art-hand-drawn-land-harvesting-wheat-5aa5d112583690.9368897615208164023613.png"
+      />
+      <Box width={isDesktopOrLaptop ? "40%" : "100%"}>
         <Heading level="2">Welcome to BuiThi Agriculture</Heading>
         <Paragraph>
           BuiThi Agriculture is fulfilling needs of Lotus seeds, Cassia /
@@ -22,11 +37,6 @@ function CompanyIntro() {
           READ MORE
         </a>
       </Box>
-      <Image
-        width="40%"
-        height="350px"
-        src="images/kisspng-drawing-agriculture-royalty-free-tractor-clip-art-hand-drawn-land-harvesting-wheat-5aa5d112583690.9368897615208164023613.png"
-      />
     </Box>
   );
 }
